bed0809eb8ed55e71cbc85b5f2e4a6e97cfda15d
OUSSAMA <oussama.choukri02@gmail.com>
2023-12-14 15:18:03 +0100

    les Tds

--- src/TD3/Ex1.java
+++ src/TD3/Ex1.java
@@ -1,4 +1,28 @@
 package TD3;
 
 public class Ex1 {
-}
+    public static void main(String[] args) {
+        PointX a=new PointX("premier point ",5);
+        a.affich();
+        a.translate(3);
+        a.affich();
+    }
+    }
+
+
+class PointX{
+    String nom;
+    double abscice;
+    public PointX(String nom,double abscice)
+    {
+        this.abscice=abscice;
+        this.nom=nom;
+    }
+    public void affich(){
+        System.out.println(this.abscice+this.nom);
+
+    }
+    public double translate(int n){
+        abscice+=n;
+        return abscice ;
+};}
